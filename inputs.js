ac847ce59704c5257e8d4964bdc0051ed357e063
Shon Hatcher <shonhatcher@hotmail.com>
2021-02-27 22:31:55 -0500

    prompts final

--- inputs.js
+++ inputs.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-inquirer
-    .prompt([
+
+const initialPrompt = [
         {
             type: 'input',
             message: "What is the team manager's name?",
@@ -23,69 +23,83 @@ inquirer
             message: "What is the employee's office number?",
             name: 'phone',
         },
+]
+    const otherPrompt = [
         {
             type: 'list',
             message: "What do you want to do next?",
             choices: ['Add an engineer','Add an intern','Finish building team'],
-            name: 'list',
+            name: 'olist',
         },
         {
             type: 'input',
             message: "What is the engineer's name?",
             name: 'eName',
-            when: (response) => response.list =='Add an engineer',
+            when: (response) => response.olist =='Add an engineer',
         },
         {
             type: 'input',
             message: "What is the engineer's employee ID?",
             name: 'eID',
-            when: (response) => response.list =='Add an engineer',
+            when: (response) => response.olist =='Add an engineer',
         },
         {
             type: 'input',
             message: "What is the engineer's email address?",
             name: 'eEmail',
-            when: (response) => response.list =='Add an engineer',
-        },  {
+            when: (response) => response.olist =='Add an engineer',
+        },  
+        {
             type: 'input',
             message: "What is the engineer's Github username?",
             name: 'eGithub',
-            when: (response) => response.list =='Add an engineer',
+            when: (response) => response.olist =='Add an engineer',
         },
         {
             type: 'input',
             message: "What is the intern's name?",
             name: 'iName',
-            when: (response) => response.list =='Add an intern',
+            when: (response) => response.olist =='Add an intern',
         },
         {
             type: 'input',
             message: "What is the intern's employee ID?",
             name: 'iID',
-            when: (response) => response.list =='Add an intern',
+            when: (response) => response.olist =='Add an intern',
         },
         {
             type: 'input',
             message: "What is the intern's email address?",
             name: 'iEmail',
-            when: (response) => response.list =='Add an intern',
+            when: (response) => response.olist =='Add an intern',
         },  
         {
             type: 'input',
             message: "What is school does the intern attend?",
             name: 'iSchool',
-            when: (response) => response.list =='Add an intern',
-        },
+            when: (response) => response.olist =='Add an intern',
+        },  
         {
             type: 'list',
             message: "What do you want to do next?",
             choices: ['Add an engineer','Add an intern','Finish building team'],
-            name: 'list',
-            loop: 'list',
-            //when: (response) => response.iSchool !='' || response.list1 == 'Add an intern',
-        },
-    ])
-    .then((response) => 
-        console.log("success")
-    );
+            name: 'olist',
+        }
+]
 
+
+function repeatCycle () {
+    inquirer.prompt(otherPrompt).then(responses =>{
+        if(responses.olist != 'Finish building team') {
+            repeatCycle()
+        } else {console.log("Team is built");
+        }
+    })
+}
+        inquirer.prompt(initialPrompt).then(responses =>{
+                repeatCycle()
+            })
+            
+ 
+
+        
